80d39eb3f1969f389baad714e6399b17fca1fa51
esilaeva <emsilaeva@gmail.com>
2025-03-21 12:43:02 +0200

    Homework 03: - pom.xml dependencies changed to Spring-boot starters; - annotation-based context changed to ConfigurableApplicationContext; - unit test adjusted to use localization-aware methods; - added application banner.

---
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,50 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+  
+  public static final String ERROR_MESSAGE = "Error reading questions";
+  
+  private static final char SEPARATOR = ';';
+  
+  private final TestFileNameProvider fileNameProvider;
+  
+  @Override
+  public List<Question> findAll() {
+    try (Reader reader = new InputStreamReader(getFile(fileNameProvider.getTestFileName()))) {
+      var questionDtoList = new CsvToBeanBuilder<QuestionDto>(reader)
+          .withType(QuestionDto.class)
+          .withSkipLines(1)
+          .withSeparator(SEPARATOR)
+          .build()
+          .parse();
+      
+      return questionDtoList.stream()
+          .map(QuestionDto::toDomainObject)
+          .toList();
+      
+    } catch (Exception e) {
+      throw new QuestionReadException(ERROR_MESSAGE, e);
+    }
+  }
+  
+  private InputStream getFile(String fileName) throws IOException {
+    
+    return new ClassPathResource(fileName).getInputStream();
+  }
+}
